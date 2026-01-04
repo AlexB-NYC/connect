fbd401c2d35d40610d3e23d16481ff3141b2429e
Alex Benson <alexwritescode.nyc@gmail.com>
2026-01-04 01:46:53 -0500

    implemented basic CRUD interactivity in list mode

--- lib/validate.sh
+++ lib/validate.sh
@@ -30,7 +30,11 @@ test_ssh_connection() {
   local user="$1" host="$2" key_path="${3:-}" insecure="${4:-0}" verbose="${5:-0}"
 
   local opts=()
-  opts+=( -o BatchMode=yes -o ConnectTimeout=7 )
+  opts+=( -o ConnectTimeout=7 )
+  if [[ -n "$key_path" ]]; then
+    opts+=( -o BatchMode=yes )
+  fi
+
 
   if (( insecure )); then
     opts+=( -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null )
